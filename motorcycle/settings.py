b43a64a5581b12f3160b5e849d77a9a8c8bcd70b
zakaria230 <zakaria@example.com>
2026-05-09 17:34:11 +0100

    Configure Vercel deployment

--- motorcycle/settings.py
+++ motorcycle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6j)fjkwb0nbgol0e%kf%5e^z6qi8h(cpmr4^&_vtg3rg3pi_m=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
